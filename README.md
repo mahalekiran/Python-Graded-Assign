0a61ab0c488411bc7aaaa14ea89d0cbc79e2cbda
Prakash Mahale <kiran.prakash.mahale@accenture.com>
2024-01-28 02:32:52 +0530

    added/modified config json files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Python-Graded-Assign
+Question 2:
+Step 1: Install psutil
+Step 2:Was getting error if tryingto interrupt the program, handled it by exception
+
+Question 3:
+Step 1: Installed Mongo DB compass
+Step 2: Created DB and Collection name. Which will be passed in the program
+Step 3: Created sample-config.config file which contains data that will be read.
+Step 4: Creted empty sample-output.jsn file
--- configuration_mgmt.py
+++ configuration_mgmt.py
@@ -1,39 +1,63 @@
-from flask import Flask, jsonify, request
-from flask_pymongo import PyMongo
+#Progam to add json file to DB
+from flask import Flask, jsonify
+import json
+import pymongo
 import configparser
 
 configp = configparser.ConfigParser()
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/your_database'
-mongo = PyMongo(app)
+
+try:
+    #Read config file
+    configp.read("sample-config.config")
+    #Mongo DB client connection string
+    mongoclient=pymongo.MongoClient('mongodb://localhost:27017/')
+except Exception as error:
+    print("Some error occurred: "+ str(error))
+finally:
+    print("Read Config File")
 
 @app.route('/')
 def index():
     return 'Hello, Flask with MongoDB!'
 
-@app.route('/users', methods=['GET'])
-def get_users():
-    users = mongo.db.users.find()
-    result = []
-    for user in users:
-        result.append({'username': user['username'], 'email': user['email']})
-    return jsonify(result)
+
+@app.route('/dbservers',methods=['GET'])
+def get_dbservers():
+        try:
+            # Get Collection object using database name
+            db = mongoclient.configMgmt
+            coll=db.get_collection('kiranmahale')
+            # Define dictionary
+            dictValues={}
+
+            # extract specific key-value pairs from the configuration file
+            for section in configp.sections():
+                keys=[]
+                #print(section)
+                for keyval in configp.items(section):
+                    keys.append(keyval)
+                dictValues[section]=keys
+                print(dictValues)
+            
+            # Save dictionary values in json file
+            with open('sample-output.json', 'a') as file:
+                file.write(json.dumps(dictValues) + '\n')
+
+            # save JSON file in the database.
+            with open('sample-output.json') as fileload:
+                file_data = json.load(fileload)
+            
+            if isinstance(file_data, list):
+                coll.insert_many(file_data)  
+            else:
+                coll.insert_one(file_data)
+
+            # Show json data
+            return jsonify(dictValues)
+        
+        except Exception as error:
+            print("Error occurred in GET api: "+ str(error))
 
 if __name__ == '__main__':
     app.run()
-
-# try:
-#     configp.read("sample-config.config")
-
-#     for section in configp.sections():
-#         dictValues={}
-#         keys=[]
-#         #print(section)
-#         for keyval in configp.items(section):
-#             keys.append(keyval)
-#         dictValues[section]=keys
-#         print(dictValues)
-# except:
-#     print("Some error occurred")
-# finally:
-#     print("Clear")
